54e3c8a2fb3852c2f00de1f27bb5348c626be225
Brian Lobdell <bri.lobdell@gmail.com>
2013-12-23 13:52:24 -0500

    Pre Heroku push, edits in bootstrap_and_customization

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # creates an about path
   get "contact" => "pages#contact"
+  get "testpage" => "pages#testpage"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
