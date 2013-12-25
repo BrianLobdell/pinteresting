d439b9e997b3e18f91dd7591385c3e875ed7da87
Brian Lobdell <bri.lobdell@gmail.com>
2013-12-25 00:35:57 -0500

    test commit. Am i on master?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Pinteresting::Application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { :host => 'omr-pinteresting-brian.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://omr-pinteresting-brian.herokuapp.com/' }
 end
